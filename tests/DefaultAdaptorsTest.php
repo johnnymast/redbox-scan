1e8f3245ddcce16bf446542694e9b0bea525df67
Johnny Mast <mastjohnny@gmail.com>
2015-12-17 20:37:40 +0100

    Fixed PHPdocs for phpunit about filesystem class

--- tests/DefaultAdaptorsTest.php
+++ tests/DefaultAdaptorsTest.php
@@ -12,7 +12,7 @@ class DefaultAdaptorsTest extends Assets\TestBase
      * This test will make sure that an PHPUnit_Framework_Error is thrown if there was no Adapter
      * set via either the constructor or via the scan method.
      *
-     * @covers                   Filesystem::write
+     * @coversDefaultClass       \Redbox\Scan\Adaptor\Filesystem
      */
     public function test_filesystem_write_should_throw_exception_on_invalid_report_argument()
     {
@@ -30,10 +30,13 @@ class DefaultAdaptorsTest extends Assets\TestBase
      * This test will make sure if no argument is passed (it defaults to null) it
      * will return false.
      *
-     * @covers                   Filesystem::write
+     * @coversDefaultClass       \Redbox\Scan\Adaptor\Filesystem
      */
     public function test_filesystem_write_fails_with_empty_argument() {
         $filesystem = new Scan\Adapter\Filesystem('somefile.yml');
         $this->assertFalse($filesystem->write());
     }
+
+
+
 }

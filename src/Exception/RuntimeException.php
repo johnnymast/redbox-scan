5f54568362082f2e1a4aa582a1adc53e1c160060
Johnny Mast <mastjohnny@gmail.com>
2015-12-17 22:04:58 +0100

    added PHPDOc's to the Redbox\Scan\Exception package

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,6 +1,13 @@
 <?php
 namespace Redbox\Scan\Exception;
 
+/**
+ * Our own implementation of of the RuntimeExcaption so we can
+ * catch it better in the tests. Not only that but i might have future plans
+ * to extend its functionality.
+ *
+ * @package Redbox\Scan\Exception
+ */
 class RuntimeException extends \RuntimeException
 {
 }

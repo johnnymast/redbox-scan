a25980f801b6ad3b38641b7d647979d44ddc7346
Johnny Mast <mastjohnny@gmail.com>
2015-12-17 22:39:23 +0100

    Deleted index.php moving to examples/basic.php

--- examples/basic.php
+++ examples/basic.php
@@ -1,13 +1,13 @@
 <?php
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 /**
  * This file is temporary to the project it will go away and make way for
  * examples directory showing you the ropes and also shows how to make custom adaptors
  * for your needs.
  */
-$path = "./";
-$scan = new Redbox\Scan\ScanService(new Redbox\Scan\Adapter\Filesystem('tmp/data.yml'));
+$path = "../";
+$scan = new Redbox\Scan\ScanService(new Redbox\Scan\Adapter\Filesystem('../tmp/data.yml'));
 $scan->index($path);
 
 

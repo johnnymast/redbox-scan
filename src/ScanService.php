081cfd95aec68442eeb21fd889ae8067850a3999
Johnny Mast <mastjohnny@gmail.com>
2015-12-24 00:45:24 +0100

    Test for travis

--- src/ScanService.php
+++ src/ScanService.php
@@ -64,7 +64,6 @@ class ScanService {
         $activePath = $path;
         $items = array();
 
-        die('@@'.$path);
         $objects = new \RecursiveIteratorIterator(new Filesystem\RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
         foreach ($objects as $name => $object) {
             if ($object->getFilename() == '.' || $object->getFilename() == '..') {

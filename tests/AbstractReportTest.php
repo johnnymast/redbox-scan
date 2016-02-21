7f1c26c0812619b4f5a9b0b7ddf31a6a82176680
Johnny Mast <mastjohnny@gmail.com>
2016-02-21 11:59:42 +0100

    redbox-scan-32 Fix grammer errors

--- tests/AbstractReportTest.php
+++ tests/AbstractReportTest.php
@@ -4,28 +4,40 @@ use Redbox\Scan\Report;
 use Redbox\Scan;
 
 /**
- * This class will run tests against the default Filesystem adapter.
+ * This class will run tests against the AbstractReport class.
  *
- * @coversDefaultClass  Scan\Adapter\Filesystem
+ * @coversDefaultClass  Scan\Report\AbstractReport
  * @package Redbox\Scan\Tests
  */
 class AbstractReportTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_set_and_get_items_return_the_same() {
+    /**
+     * Tests that set and get Items files return the same.
+     */
+    public function test_set_and_get_items_return_the_same()
+    {
         $report = new Report\Report();
         $items  = array('a' => 'b');
         $report->setItems($items);
         $this->assertEquals($items, $report->getItems());
     }
 
-    public function test_set_and_get_newfiles_return_the_same() {
+    /**
+     * Tests that set and get NewFiles files return the same.
+     */
+    public function test_set_and_get_newfiles_return_the_same()
+    {
         $report = new Report\Report();
         $items  = array('a' => 'b');
         $report->setNewfiles($items);
         $this->assertEquals($items, $report->getNewfiles());
     }
 
-    public function test_set_and_get_modified_files_return_the_same() {
+    /**
+     * Tests that set and get Modified files return the same.
+     */
+    public function test_set_and_get_modified_files_return_the_same()
+    {
         $report = new Report\Report();
         $items  = array('a' => 'b');
         $report->setModifiedFiles($items);
